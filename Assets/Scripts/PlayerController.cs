cdc1232e85c1cd34138b51c62f59e933e31e6187
KoalitsiyaSV <chyoung118@gmail.com>
2023-04-05 19:59:05 +0900

    test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
         playerAnimator = GetComponent<Animator>();
         playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
-
+    
     // Update is called once per frame
     void Update() {
         
