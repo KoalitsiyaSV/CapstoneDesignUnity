15522c32684b657fc0a697c13b4932e5e56464f4
Koalitsiya <chyoung118@gmail.com>
2023-04-07 21:58:46 +0900

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer playerSpriteRenderer;
 
     public float speed = 8f;
+    public float jumpForce = 10f;
+    public bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,15 +21,36 @@ public class PlayerController : MonoBehaviour
     
     // Update is called once per frame
     void Update() {
-        
-    }
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+                playerRigidbody.velocity = Vector2.up * jumpForce;
+                isGrounded = false;
+            }
+        }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         float xMove = Input.GetAxisRaw("Horizontal");
 
         float xSpeed = xMove * speed;
 
-        Vector3 newVelocity = new Vector3(xSpeed, 0f, 0f);
-        playerRigidbody.velocity = newVelocity;
+        playerRigidbody.velocity = new Vector2(xMove * speed, playerRigidbody.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Slope")) {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision, true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.CompareTag("Slope")) {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Slope")) {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision, false);
+        }
     }
 }
