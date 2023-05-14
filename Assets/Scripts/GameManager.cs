7a317cac3e8fed8cf48a118cd183b698bc893d0c
KoalitsiyaSV <chyoung118@gmail.com>
2023-05-14 22:13:55 +0900

    7th

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject MenuUI;
+    public GameObject InGameUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,31 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //인게임 메뉴 버튼 클릭 시 이벤트
+    public void OnClickMenuBtn()
+    {
+        MenuUI.SetActive(true);
+        InGameUI.SetActive(false);
+    }
+
+    //메뉴창->게임 종료 버튼
+    public void OnClickExitBtn()
+    {
+        OnApplicationQuit();
+    }
+
+    //메뉴창->메뉴 닫기 버튼
+    public void OnClickCloseBtn()
+    {
+        MenuUI.SetActive(false);
+        InGameUI.SetActive(true);
+    }
+
+    //종료 대충
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
     }
 }
