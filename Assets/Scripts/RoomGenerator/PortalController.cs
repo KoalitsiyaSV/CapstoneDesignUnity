178c944b754a39e7aa3d2efbd1b68952a7b45011
ReynoldZZZ <reynolds7@naver.com>
2023-11-29 20:26:38 +0900

    1129TempUpdate

--- Assets/Scripts/RoomGenerator/PortalController.cs
+++ Assets/Scripts/RoomGenerator/PortalController.cs
@@ -12,20 +12,21 @@ public class PortalController : MonoBehaviour
         PortalManager = GameObject.Find("PortalManager");
         canPlayerTeleport = false;
     }
-
     private void Update() {
         if (canPlayerTeleport && Input.GetKeyDown(KeyCode.UpArrow)) {
-            Debug.Log(this.name);
-            PortalManager.GetComponent<PortalManager>().PlayerTeleportation(this.name);
+            if(this.gameObject.name.Contains("up") || this.gameObject.name.Contains("down")) {
+                PortalManager.GetComponent<PortalManager>().PlayerTeleportation(this.name);
+            }
+            else
+                PortalManager.GetComponent<PortalManager>().PlayerTeleportation(this.gameObject);
 
         } // Player가 Portal에 접촉하고, W를 누르면 입장한 Portal의 이름을 매개변수로 PlayerTeleportation을 호출.
     }
-
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             canPlayerTeleport = true;
         }
-    } 
+    }
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             canPlayerTeleport = false;
