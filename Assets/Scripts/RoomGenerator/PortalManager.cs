a78a85dc0533e992102c4e12fdb47786b1dc9c52
ReynoldZZZ <reynolds7@naver.com>
2023-11-30 01:24:47 +0900

    1130Update
    Dungeon almost done

--- Assets/Scripts/RoomGenerator/PortalManager.cs
+++ Assets/Scripts/RoomGenerator/PortalManager.cs
@@ -21,22 +21,6 @@ public class PortalManager : MonoBehaviour
         playerSpawnPoint = new Vector2Int(GridSize.x / 2, GridSize.y / 2);
         currentPlayerPosition = playerSpawnPoint;
     }
-
-
-    public void PlayerTeleportation(string entryPortalName) {
-        if (entryPortalName == "upPortal") { // Player가 위 Portal로 입장한 경우
-            currentPlayerPosition.y++;
-            Vector3 destination = RoomManager.GetComponent<RoomManager>().teleportRoom(currentPlayerPosition, "down");
-            Player.transform.SetPositionAndRotation(destination, Quaternion.identity);
-            // 현재 위치에서 위쪽 Room의 아래 Portal을 목적지로 설정하고 Player을 이동
-        }
-        else if (entryPortalName == "downPortal") { //Player가 좌측 Portal로 입장한 경우
-            currentPlayerPosition.y--;
-            Vector3 destination = RoomManager.GetComponent<RoomManager>().teleportRoom(currentPlayerPosition, "up");
-            Player.transform.SetPositionAndRotation(destination, Quaternion.identity);
-            // 현재 위치에서 좌측 Room의 우측 Portal을 목적지로 설정하고 Player을 이동
-        }
-    }
     public void PlayerTeleportation(GameObject entry) {
         if(entry.CompareTag("Left")) {
             currentPlayerPosition.x--;
@@ -52,6 +36,24 @@ public class PortalManager : MonoBehaviour
             Room destination = RoomManager.GetComponent<RoomManager>().Teleport(currentPlayerPosition);
             foreach (Transform objects in destination.transform) {
                 if (objects.gameObject.CompareTag("Left")) {
+                    Player.transform.position = objects.gameObject.transform.position;
+                }
+            }
+        }
+        else if (entry.CompareTag("Up")) {
+            currentPlayerPosition.y++;
+            Room destination = RoomManager.GetComponent<RoomManager>().Teleport(currentPlayerPosition);
+            foreach (Transform objects in destination.transform) {
+                if (objects.gameObject.CompareTag("Down")) {
+                    Player.transform.position = objects.gameObject.transform.position;
+                }
+            }
+        }
+        else if (entry.CompareTag("Down")) {
+            currentPlayerPosition.y--;
+            Room destination = RoomManager.GetComponent<RoomManager>().Teleport(currentPlayerPosition);
+            foreach (Transform objects in destination.transform) {
+                if (objects.gameObject.CompareTag("Up")) {
                     Player.transform.position = objects.gameObject.transform.position;
                 }
             }
